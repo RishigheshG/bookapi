732650bf872c7d8bac4afbc348ae156cc38b38b2
XDawnBreakerX <grishighesh@gmail.com>
2021-11-19 15:34:52 +0530

    added .env

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 //database
 const Database = require("./database")
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to MongoDB!"))
+.catch((err) => {
+  console.log(err);
+});
+
 
 //initialization
 const OurAPP = express();
